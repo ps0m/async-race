f2699a17d636b8b99f71ab9afcc5681526866dfd
ps0m <ps0m@tut.by>
2022-10-17 13:50:18 +0300

    fix: fix mistakes eslint

--- my-app/src/components/UI/Button/Button.tsx
+++ my-app/src/components/UI/Button/Button.tsx
@@ -1,24 +1,30 @@
-// import React from 'react';
-import classes from "./Button.module.css";
+import { FC, MouseEvent, ReactNode } from 'react';
+import classes from './Button.module.css';
 
 interface MyButtonProps {
   className?: string
-  onClick(e: React.MouseEvent<HTMLButtonElement>): void;
+  onClick(e: MouseEvent<HTMLButtonElement>): void;
   isActive: boolean;
-  children?: React.ReactNode
+  children?: ReactNode
   disabled?: boolean
 }
 
-const MyButton = ({ className, onClick, isActive, children, disabled }: MyButtonProps) => {
-
+const MyButton: FC<MyButtonProps> = ({
+  className, onClick, isActive, children, disabled,
+}) => {
   const cardClasses = [classes.button, className];
 
   if (isActive) {
-    cardClasses.push(classes.button_select)
+    cardClasses.push(classes.button_select);
   }
 
   return (
-    <button onClick={onClick} disabled={disabled} className={cardClasses.join(" ")} >
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+      className={cardClasses.join(' ')}
+    >
       {children}
     </button>
   );
